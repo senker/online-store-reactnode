f9cd614deadd5f7a348383dd8549bb4c5f71a6c0
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-04-11 14:27:06 +0200

    adding navigation, routing, routes, route, redirect(outdated and switched to Navigate), approuter

--- online-store-react-node/client/src/App.js
+++ online-store-react-node/client/src/App.js
@@ -1,11 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 
 const App = () => {
   return (
-    <div>
-      Working!
-    </div>
-  )
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  );
 };
-
 
 export default App;
