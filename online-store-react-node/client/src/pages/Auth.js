30e6f32558b1e53369d6a13f18c64dadeffec946
Vladimir Rotaru <31960682+senker@users.noreply.github.com>
2022-04-26 23:34:51 +0200

    interraction with server, setting up axios p1

--- online-store-react-node/client/src/pages/Auth.js
+++ online-store-react-node/client/src/pages/Auth.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card, Container, Form } from "react-bootstrap";
 import { NavLink, useLocation } from "react-router-dom";
+import { login, registration } from "../http/userAPI";
 import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
   const location = useLocation();
   const isLogin = location.pathname === LOGIN_ROUTE;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const click = async () => {
+    if (isLogin) {
+      const response = await login();
+    } else {
+      const response = await registration(email, password);
+      console.log(response);
+    }
+  };
 
   return (
     <Container
@@ -15,10 +27,18 @@ const Auth = () => {
       <Card style={{ width: 600 }} className="p-5">
         <h2 className="m-auto">{isLogin ? "Authorization" : "Registration"}</h2>
         <Form className="d-flex flex-column">
-          <Form.Control className="mt-3" placeholder="Enter your email ..." />
+          <Form.Control
+            className="mt-3"
+            placeholder="Enter your email ..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <Form.Control
             className="mt-3"
             placeholder="Enter your password ..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
           />
         </Form>
         <div className="d-flex justify-content-between mt-3 ps-3 pe-3">
@@ -33,7 +53,7 @@ const Auth = () => {
             </div>
           )}
 
-          <Button variant={"outline-success"}>
+          <Button variant={"outline-success"} onClick={click}>
             {isLogin ? "Enter" : "Register"}
           </Button>
         </div>
